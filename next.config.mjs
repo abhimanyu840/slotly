e4758f6921f4e6f23f2fc786b9b8a9391e05ab95
Abhimanyu Kumar <akabhimanyukumar111@gmail.com>
2024-10-13 10:27:51 +0530

    webpack configuration for framer motion

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com','assets.aceternity.com'],
+        domains: ['images.unsplash.com', 'assets.aceternity.com'],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // Exclude Framer Motion from the server-side bundle
+            config.resolve.alias['framer-motion'] = 'framer-motion/dist/framer-motion';
+        }
+        return config;
     },
 };
 
